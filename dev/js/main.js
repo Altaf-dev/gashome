f41ddf5fc84d67cd7ff08ae08a966171b1051f26
Altaf-dev <altaf090486@gmail.com>
2020-06-04 16:12:26 +0600

    add:project

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,9 +1,3 @@
-// const title = document.getElementById('title');
-// document.querySelector('.title').onclick = function (){
-//     title.classList.toggle('title');
-//     title.classList.toggle('title_click');
-// }
-
 var acc = document.getElementsByClassName("city-tel");
 var i;
 for (i = 0; i < acc.length; i++){
@@ -13,14 +7,7 @@ for (i = 0; i < acc.length; i++){
     }
 }
 
-// var menu = document.getElementsByClassName("menu");
-// var x;
-// for (x = 0; x < menu.length; x++){
-//     menu[x].onclick = function () {
-//         this.classList.toggle("bt");
-//
-//     }
-// }
+
 document.querySelector('.menu-btn').addEventListener('click', function(){
     document.querySelector('.menu-btn').classList.toggle('bt');
     document.querySelector('.nav-menu_block').classList.toggle('open');
